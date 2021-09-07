8272c13646e913baa7a918ac415eb6e601818faf
Ezequiel Dhonatan <Contato@ezequieldhonatan.com.br>
2021-09-07 18:37:46 -0300

    Installation - RabbitMQ [CloudAMQP]

---
+++ resources/views/mails/company/created/welcome.blade.php
@@ -0,0 +1,11 @@
+@component( 'mail::message' )
+
+# Olá
+
+Seja muito bem vindo(a) ao nosso sistema!
+
+Obrigado,<br>
+
+{{ config( 'app.name' ) }}
+
+@endcomponent
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Jobs\Company\Created\WelcomeJob;
+
 Route::get( '/', function () {
+
     return response()->json( [ 'message' => 'success' ] );
-});
+
+}); //
+
+Route::get( '/test', function () {
+
+    WelcomeJob::dispatch( 'Test@test.com' );
+
+    return response()->json( [ 'message' => 'success' ] );
+
+}); //
